d36ec4ff49052cf01522a4c20d04430c56e1952a
CHRISTOPHER GABRIEL <58720335+CPG19040@users.noreply.github.com>
2026-05-07 17:44:57 +0800

    Delete CAI_System directory

--- CAI_System/CAI_Admin/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def setNameAndPosition(request):
-    return render(request, 'Home.html', {'name': 'Christopher', 'position': 'Admin'})
--- CAI_System/CAI_Student/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CaiStudentConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "CAI_Student"
